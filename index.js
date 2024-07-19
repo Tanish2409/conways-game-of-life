3e8df24e784f486842dcca6af5790b33218cb02c
Tanish Agarwal <tanish@byldd.com>
2024-07-20 03:01:53 +0530

    refactor: optimise update ui logic
    - instead of recreating the whole grid with new elements, now updating the previous one's based on the state.

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const ROWS = 50;
-	const COLS = 50;
+	const ROWS = 40;
+	const COLS = 40;
 
-	let intervalId = null;
+	/**
+	 * @description To globally store the setInterval id when the game is started.
+	 * Will be used to stop the game
+	 */
+	let startGameIntervalId = null;
 
+	/**
+	 * @description Wrapper element to store rows and columns of the cells
+	 * @type {HTMLTableElement}
+	 */
 	const canvas = document.getElementById('canvas');
 
+	/**
+	 * @type {HTMLButtonElement}
+	 */
 	const startBtn = document.getElementById('start-btn');
 	startBtn.addEventListener('click', startGame);
 
+	/**
+	 * @type {HTMLButtonElement}
+	 */
 	const stopBtn = document.getElementById('stop-btn');
 	stopBtn.addEventListener('click', stopGame);
 
-	let initialCellGrid = Array.from({ length: COLS }, () =>
-		new Array(ROWS).fill(0)
+	/**
+	 * @description Initial ROWS x COLS grid to be rendered in the UI
+	 * @type {number[][]}
+	 */
+	let initialCellGrid = Array.from({ length: ROWS }, () =>
+		new Array(COLS).fill(0)
 	);
 
 	/**
-	 * @description - This created a ROWS x COLS grid in the browser
+	 * @description - This is to mark(populate)/unmark(unpopulate) a single cell.
+	 *
+	 * @param {MouseEvent} event
+	 */
+	function handleCellClick(event) {
+		const row = event.target.dataset.row;
+		const col = event.target.dataset.col;
+
+		initialCellGrid[row][col] = initialCellGrid[row][col] === 0 ? 1 : 0;
+
+		event.currentTarget.classList.toggle('selected');
+	}
+
+	/**
+	 * @description - This creates an initial ROWS x COLS grid in the browser
 	 * and attaches appropriate event listeners to it.
 	 */
-	function drawGrid(gridState) {
-		canvas.innerHTML = '';
-
+	function renderUI() {
 		for (let row = 0; row < ROWS; row++) {
 			const cellRow = document.createElement('tr');
 			cellRow.classList.add('cell-row');
@@ -34,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 				cell.dataset.row = row;
 				cell.dataset.col = col;
 
-				if (gridState[row][col] === 1) {
+				if (initialCellGrid[row][col] === 1) {
 					cell.classList.add('selected');
 				}
 
@@ -47,23 +77,41 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	function calcCellState({ gridState, coords }) {
-		// console.log(gridState);
+	/**
+	 * @description updates all the cells state in the UI based on the the current grid state
+	 * @param {number[][]} gridState - The state based on which new UI will be rendered
+	 */
+	function updateUI(gridState) {
+		const cells = document.querySelectorAll('.cell');
 
+		cells.forEach((cell) => {
+			const row = parseInt(cell.dataset.row);
+			const col = parseInt(cell.dataset.col);
+
+			if (gridState[row][col]) {
+				cell.classList.add('selected');
+			} else {
+				cell.classList.remove('selected');
+			}
+		});
+	}
+
+	function calcCellState({ gridState, coords }) {
 		let neighbours = 0;
 
 		// calculate the number of neighbours for current cell
 		for (let i = -1; i <= 1; i++) {
 			for (let j = -1; j <= 1; j++) {
 				if (
+					// to check if the neighbour is inside the bounds of the grid
 					coords.row + i >= 0 &&
 					coords.row + i < ROWS &&
 					coords.row + j >= 0 &&
-					coords.row + j < COLS
+					coords.row + j < COLS &&
+					// to check if it's not the current cell
+					(i !== 0 || j !== 0)
 				) {
-					if (i !== 0 || j !== 0) {
-						neighbours += gridState[coords.row + i][coords.col + j];
-					}
+					neighbours += gridState[coords.row + i][coords.col + j];
 				}
 			}
 		}
@@ -83,6 +131,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	function calcUpdatedGridState(currGridState) {
+		// creating a copy because the complete next state is calculated from the whole current state
+		// thus can't update the current state one by one. It needs to be updated as a whole.
 		const updatedState = JSON.parse(JSON.stringify(currGridState));
 
 		for (let i = 0; i < ROWS; i++) {
@@ -104,30 +154,17 @@ document.addEventListener('DOMContentLoaded', () => {
 		startBtn.classList.add('hide');
 		stopBtn.classList.remove('hide');
 
-		intervalId = setInterval(() => {
+		startGameIntervalId = setInterval(() => {
 			initialCellGrid = calcUpdatedGridState(initialCellGrid);
-			drawGrid(initialCellGrid);
+			updateUI(initialCellGrid);
 		}, 200);
 	}
 
 	function stopGame() {
 		startBtn.classList.remove('hide');
 		stopBtn.classList.add('hide');
-		clearInterval(intervalId);
+		clearInterval(startGameIntervalId);
 	}
 
-	/**
-	 * @description - This is to mark/unmark cells while the game has not started.
-	 * @param {MouseEvent} event
-	 */
-	function handleCellClick(event) {
-		const row = event.target.dataset.row;
-		const col = event.target.dataset.col;
-
-		initialCellGrid[row][col] = initialCellGrid[row][col] === 0 ? 1 : 0;
-
-		event.currentTarget.classList.toggle('selected');
-	}
-
-	drawGrid(initialCellGrid);
+	renderUI();
 });
